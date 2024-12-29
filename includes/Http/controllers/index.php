c4bada746dd6ee982ab554dec8b6547610103674
keiner <keinermendoza@gmail.com>
2024-12-28 21:43:34 -0400

    feat: creating public page for exchanges

--- includes/Http/controllers/index.php
+++ includes/Http/controllers/index.php
@@ -1,5 +1,19 @@
-<?php 
+<?php
+namespace Http;
+use Core\App;
+use Core\Database;
+
+$db = App::resolve(Database::class);
+
+$sqlExchanges = require base_path("Http/controllers/api/exchangerates/sql.php");
+
+$exchanges = $db->query($sqlExchanges)->fetchAll();
+$infosite = $db->query("SELECT * FROM info_site LIMIT 1")->fetch();
+$social = $db->query("SELECT * FROM social_accounts LIMIT 1")->fetch();
 
 view("index.view.php", [
+    "exchanges" => $exchanges,
+    "infosite" => $infosite,
+    "social" => $social,
     "title" => "Home"
 ]);
